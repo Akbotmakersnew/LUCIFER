906c623270e8220753d4eaeddb11b8e4ede37a10
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-01-15 12:03:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akbotmakersnew/LUCIFER.git /LUCIFER
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUCIFER
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
