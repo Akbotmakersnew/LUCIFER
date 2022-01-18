423587281637443c9e8b413a804a059b9ae131cd
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-01-18 08:11:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LUCIFER
 fi
-cd /EvaMaria
+cd /LUCIFER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
