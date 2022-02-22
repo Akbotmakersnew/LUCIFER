bafb6f20ebcfc012a2e28ff2e18c54ffcf8c5d67
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-02-22 18:59:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,11 +4,13 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ADMINS_TXT = """<b>NOTE:</b>
-Here is LUCIFER bot admins
-• @IAM_A_JOKER
-• @JINN_SULTHAN
-• @psychopath_here
-Thanks For Using Me."""
+- LUCIFER is a open source project. 
+- ADMINS are: @IAM_A_JOKER
+              @JINN_SULTHAN
+              @psychopath_here
+
+<b>DEVS:</b>
+- <a href=https://t.me/Ak_Bot_SupportGroup>Team AkBotDevs</a>"""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Ak_Bot_SupportGroup>Team AkBotDevs</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
