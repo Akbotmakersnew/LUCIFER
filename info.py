a0fb2dda49db7f28577d958efb85902399e7990e
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-12-12 20:24:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,6 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
+PORT = environ.get("PORT", "8080")
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
