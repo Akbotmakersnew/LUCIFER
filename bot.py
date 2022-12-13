be00ba7ae3ef0c195c63b34124ee1bdbe0df1c78
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-12-13 19:32:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            name=LUCIFER,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
